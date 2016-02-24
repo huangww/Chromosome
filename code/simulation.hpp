c306ba3a71513d701521f1481dd24723caf62a04
huangwenwen <huangww87@gmail.com>
2016-02-24 02:24:29 +0100

    Add input class
    Former-commit-id: ff7ba45cc38ccff33d48873605ce58516eb3b329

--- code/simulation.hpp
+++ code/simulation.hpp
@@ -8,18 +8,11 @@ public:
     Simulation ();
     virtual ~Simulation ();
 
-<<<<<<< HEAD
-    class Input *input;         // input scripting
-    class Box *box;             // box to do simulation
-    class Output *output;       // output streams
-
-=======
     class Input *input;     // input scripting
     class Box *box;         // box to do the simulation
     class Output *output;   // output streams
     
     void init(std::string simuName);
->>>>>>> 06d6606705b9676fb9bda9404137c07e1b7d806c
     virtual void print() = 0;
     virtual void run() = 0;
 };
