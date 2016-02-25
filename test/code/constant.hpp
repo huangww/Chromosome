b72866ddaa889ac3d6b264a3a4ba9a6733374f87
huangwenwen <huangww87@gmail.com>
2016-02-25 02:30:46 +0100

    asep code is done but still have to debug
    Former-commit-id: fe9330d71e2d260584248f99e4ee340791dbb9a1

---
+++ test/code/constant.hpp
@@ -0,0 +1,8 @@
+#ifndef CONSTANT_HPP_QF8VSGA4
+#define CONSTANT_HPP_QF8VSGA4
+
+#define DIM 3
+#define PI 3.14159265359
+
+
+#endif /* end of include guard: CONSTANT_HPP_QF8VSGA4 */
---
+++ test/code/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include "simulation.hpp"
+#include "input.hpp"
+
+int main(int argc, char *argv[])
+{
+    int startTime = time(NULL);
+   
+    Simulation *simu = new Simulation;
+
+    simu->input->getInput(argc, argv);
+    simu->input->file();
+
+    simu->init();
+    simu->run();
+
+    delete simu;
+
+    int endTime = time(NULL);
+    int elapsedTime = endTime - startTime;
+    std::cout << "Runing Time: " << elapsedTime 
+    << "seconds" << std::endl;
+    return 0;
+}
