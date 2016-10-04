7cc22dc1ff9a033ac86b2ec2b9df0bef9789a27e
Wenwen Huang <huangww87@gmail.com>
2016-10-04 18:57:29 +0200

    Add Eigen to code and change .gitignore
    Former-commit-id: cdd4cd69575ea98edde32e18ab95ffcf34251163

--- code/rod.hpp
+++ code/rod.hpp
@@ -4,8 +4,34 @@
 #include "Eigen/Sparse"
 #include "input.hpp"
 #include "bead.hpp"
+#include "Eigen/unsupported/Eigen/NonLinearOptimization"
 
 typedef Eigen::SparseMatrix<double> SpMatD;
+
+template<typename _Scalar, int NX=Dynamic, int NY=Dynamic>
+struct Functor
+{
+  typedef _Scalar Scalar;
+  enum {
+    InputsAtCompileTime = NX,
+    ValuesAtCompileTime = NY
+  };
+  typedef Matrix<Scalar,InputsAtCompileTime,1> InputType;
+  typedef Matrix<Scalar,ValuesAtCompileTime,1> ValueType;
+  typedef Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+
+  const int m_inputs, m_values;
+
+  Functor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+  Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+
+  int inputs() const { return m_inputs; }
+  int values() const { return m_values; }
+
+  // you should define that in the subclass :
+//  void operator() (const InputType& x, ValueType* v, JacobianType* _j=0) const;
+};
+
 
 class Rod
 {
@@ -43,6 +69,7 @@ private:
     void jacobian(double *x, double *A, double *B);
     void inverse(double *A, int N);
     void solverNewton(double *x);
+    void solverHydrj(double *x);
     double detBandMetric(int n, double *coeff);
 
     // parameters
